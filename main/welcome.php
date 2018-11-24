b8e0bab807e4ec7a879f9cd4a0780d90e3433542
Khubaib97 <khubaib.ahmed@hotmail.com>
2018-11-24 13:23:28 +0500

    sat

--- main/welcome.php
+++ main/welcome.php
@@ -15,17 +15,31 @@
    <br />
    <br />
    <br />
-   <br />
-   <br />
    <body>
       <h1 align="center">Welcome, <?php echo $login_session; ?>!</h1>
 	<table>
 	<tr>
 	<div align="center" style="margin: 20px">
 	<a href='../customer/table.php' class='btn btn-primary'>Customer</a>
+	</div>
+	</tr>
+	<tr>
+	<div align="center" style="margin: 20px">
 	<a href='../salesperson/table.php' class='btn btn-primary'>Salesperson</a>
+	</div>
+	</tr>
+	<tr>
+	<div align="center" style="margin: 20px">
 	<a href='../product/table.php' class='btn btn-primary'>Product</a>
+	</div>
+	</tr>
+	<tr>
+	<div align="center" style="margin: 20px">
 	<a href='../salesorder/table.php' class='btn btn-primary'>Salesorder</a>
+	</div>
+	</tr>
+	<tr>
+	<div align="center" style="margin: 20px">
 	<a href='../user/table.php' class='btn btn-primary'>User</a>
 	</div>
 	</tr>
